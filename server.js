d5ec5a26d9216fea679a7f3ff6610b457705782a
MgenGlder <mgen.glder@gmail.com>
2017-12-15 21:02:07 -0500

    server fix to get the client angular code to be served correctly.

--- server.js
+++ server.js
@@ -6,14 +6,11 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var cookieparser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./app_api/models/db');
+//require('./app_api/models/db');
 var fs = require('fs');
 
 
-
-
-
-var routesApi = require('./app_api/routes/index');
+//var routesApi = require('./app_api/routes/index');
 
 app.use(express.static(__dirname + '/dist'));
 
