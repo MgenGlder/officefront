a1495c3fa36b81f294d38ac6a6996673c3da2b7d
MgenGlder <mgen.glder@gmail.com>
2017-12-18 18:13:06 -0500

    Enabling api endpoint that will be used for database queries.

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var cookieparser = require('cookie-parser');
 var bodyParser = require('body-parser');
-require('./src/app_api/models/db');
 var fs = require('fs');
-
-
 var routesApi = require('./src/app_api/routes/index');
+require('./src/app_api/models/db');
+
 
 app.use(express.static(__dirname + '/dist'));
+app.use("/api", routesApi);
 
 app.listen(process.env.PORT || 8080);
