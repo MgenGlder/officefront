df2463d6e82c70abfb7034808cdbf1b662a2e280
MgenGlder <mgen.glder@gmail.com>
2017-12-19 19:12:38 -0500

    Removed unneeded cookie parser for now. Added body parser because it is needed for api to parse incoming requests.

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ const express = require('express');
 const app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
-var cookieparser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var fs = require('fs');
 require('./src/app_api/models/db');
 var routesApi = require('./src/app_api/routes/index');
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(express.static(__dirname + '/dist'));
 app.use("/api", routesApi);
