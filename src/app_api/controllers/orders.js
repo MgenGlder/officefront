2e82e4af92dcabf60045e8a861eebb56e112fea1
MgenGlder <mgen.glder@gmail.com>
2017-12-20 17:59:10 -0500

    createOrder, getOrder, and getAllOrders api implementation

--- src/app_api/controllers/orders.js
+++ src/app_api/controllers/orders.js
@@ -7,21 +7,73 @@ var sendJsonResponse = function (res, status, content) {
 
 }
 
-function createOrder (req, res) {
-    sendJsonResponse (res, 200, { 
-        "status": "ok", 
-        "data": "createOrder api called."
-    });
+function createOrder(req, res) { //generic, TODO: create a unique function for each type of test.
+    Order
+        .create({
+            "type"          : req.body.type,
+            "reason"        : req.body.reason,
+            "location"      : req.body.location,
+            "notes"         : req.body.notes,
+            "dateOfVisit"   : req.body.dateOfVisit,
+            "visitingDoctor": req.body.visitingDoctor,
+            "reporter"      : req.body.reporter
+        }, (err, order) => {
+            if (err) {
+                sendJsonResponse(res, 400, {
+                    "status" : "error",
+                    "message": err
+                });
+            }
+            else {
+                sendJsonResponse(res, 200, {
+                    "status" : "ok",
+                    "message": "Order was created successfully"
+                });
+            }
+        })
 }
 
-function getOrder (req, res) {
-    sendJsonResponse (res, 200, {
-        "status": "ok",
-        "data": "getOrder api called"
-    })
+function getOrder(req, res) {
+    Order
+        .findOne({
+            "type"          : req.body.type,
+            "dateOfVisit"   : req.body.dateOfVisit,
+            "visitingDoctor": req.body.visitingDoctor,
+        })
+        .exec((err, orders) => {
+            if (err) {
+                sendJsonResponse(res, 400, {
+                    "status" : "error",
+                    "message": err
+                })
+            }
+            else {
+                sendJsonResponse(res, 200, {
+                    "status" : "ok",
+                    "message": orders
+                })
+            }
+        });
 }
 
+function getAllOrders(req, res) {
+    Order
+        .find({}, (err, orders) => {
+            if (err) {
+                sendJsonResponse(res, 400, {
+                    "status" : "error",
+                    "message": err
+                })
+            }
+            else {
+                sendJsonResponse(res, 200, {
+                    "status" : "ok",
+                    "message": orders
+                })
+            }
+        })
+}
 module.exports = {
-    getOrder   : getOrder,
+    getOrder: getOrder,
     createOrder: createOrder
 }
