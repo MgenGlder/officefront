7d82012f56569c1a9c03ba06d8f20eb8049f1877
MgenGlder <mgen.glder@gmail.com>
2017-12-19 19:13:45 -0500

    Adding dateOfBirth field to the patient schema. We should know to reduce duplicate patients.

--- src/app_api/models/patients.js
+++ src/app_api/models/patients.js
@@ -3,6 +3,7 @@ var mongoose = require("mongoose");
 var patientSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
+    dateOfBirth: Date
 })
 
 mongoose.model("Patient", patientSchema);
