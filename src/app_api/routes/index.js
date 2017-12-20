e1a9cd0f8c7da20f51a03427881a5fa3488d29c3
MgenGlder <mgen.glder@gmail.com>
2017-12-20 17:59:42 -0500

    Update to the api endpoint name, since only creating one patient- making the api name correspond intuitively to that.

--- src/app_api/routes/index.js
+++ src/app_api/routes/index.js
@@ -5,7 +5,7 @@ var ctrlPatients = require('../controllers/patients');
 var ctrlOrders = require('../controllers/orders');
 
 router.get('/patient', ctrlPatients.getPatient);
-router.post('/patients', ctrlPatients.createPatient);
+router.post('/patient', ctrlPatients.createPatient);
 router.get('/patients', ctrlPatients.getAllPatients);
 router.get('/orders', ctrlOrders.getOrder);
 router.post('/oders', ctrlOrders.createOrder);
