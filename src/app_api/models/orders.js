c950b3f985b83c28b087727230973495a56dcdf8
MgenGlder <mgen.glder@gmail.com>
2017-12-20 19:01:16 -0500

    adding unique identifier to the order schema so we can identify it through access to one index and one field.

--- src/app_api/models/orders.js
+++ src/app_api/models/orders.js
@@ -1,9 +1,16 @@
+import { String } from "core-js/library/web/timers";
+
 var mongoose = require("mongoose");
 
 var orderSchema = new mongoose.Schema({
-    type: String,
-    dateOfVisit: Date,
-    visitingDoctor: String,
+    type              : String,
+    dateOfVisit       : Date,
+    visitingDoctor    : String,
+    uniqueID          : String,
+    patientFirstName  : String,
+    patientlastName   : String,
+    patientDateOfBirth: Date
+    
 
 });
 
