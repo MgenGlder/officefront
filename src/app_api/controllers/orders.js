deb9f46aa1b82575dc5643ae80c380743cbaf81b
MgenGlder <mgen.glder@gmail.com>
2017-12-22 20:51:52 -0500

    Uodating create order method of api to create a reference of the patient object in the order as part of the function logic.

--- src/app_api/controllers/orders.js
+++ src/app_api/controllers/orders.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
-var Order = mongoose.model("Order");
+var Order    = mongoose.model("Order");
+var Patient  = mongoose.model("Patient");
 
 var sendJsonResponse = function (res, status, content) {
     res.status(status);
@@ -8,19 +9,42 @@ var sendJsonResponse = function (res, status, content) {
 }
 
 function createOrder(req, res) { //generic, TODO: create a unique function for each type of test.
+    var associatedPatient;
+    Patient
+        .findOne({
+            firstName: req.boy.patientFirstName,
+            lastName: req.body.patientLastName,
+            dateOfBirth: req.body.patientDateOfBirth
+        }, (err, patient) => {
+            if (err) {
+                sendJsonResponse(res, 400, {
+                    "status" : "error",
+                    "message": err
+                })
+                console.log("could not find the patient");
+                return;
+            }
+            else {
+                associatedPatient = patient;
+                sendJsonResponse(res, 200, {
+                    "status" : ok,
+                    "message": patient
+                })
+            }
+        })
     Order
         .create({
-            "patientFirstName": req.body.patientFirstName,
-            "patientLastName": req.body.patientLastName,
-            "patientDateOfBirth": req.body.patientDateOfBirth,
-            "type": req.body.type,
-            "reason": req.body.reason,
-            "location": req.body.location,
-            "notes": req.body.notes,
-            "dateOfVisit": req.body.dateOfVisit,
+            "patient"       : associatedPatient,
+            "type"          : req.body.type,
+            "reason"        : req.body.reason,
+            "location"      : req.body.location,
+            "notes"         : req.body.notes,
+            "dateOfVisit"   : req.body.dateOfVisit,
             "visitingDoctor": req.body.visitingDoctor,
-            "reporter": req.body.reporter,
-            "uniqueID": `${req.body.patientLastName}${req.body.patientFirstName}${req.body.patientDateOfBirth}${req.body.type}${req.body.dateOfVisit}${req.body.visitingDoctor}`
+            "reporter"      : req.body.reporter,
+            "uniqueID"      : `${req.body.patientLastName}${req.body.patientFirstName}${req.body.patientDateOfBirth}${req.body.type}${req.body.dateOfVisit}${req.body.visitingDoctor}`
+            //not the best solution because the patient's name might change.
+            //might have to use mongo functions to do validation before order updates.
         }, (err, order) => {
             if (err) {
                 sendJsonResponse(res, 400, {
@@ -77,6 +101,28 @@ function getAllOrders(req, res) {
                 })
             }
         })
+}
+
+function getAllOrdersSpecificPatient(req, res){
+    Order
+        .find({
+                'patient.firstName'  : req.query.patientFirstName,
+                'patient.lastName'   : req.body.patientLastName,
+                'patient.dateOfBirth': req.body.patientDateOfBirth
+            }, (err, allPatientOrders) => {
+                if (err) {
+                    sendJsonResponse(res, 400, {
+                        "status": "error",
+                        "message": err
+                    })
+                }
+                else {
+                    sendJsonResponse(res, 200, {
+                        "status": "ok",
+                        "message": allPatientOrders
+                    });
+                }
+            })
 }
 module.exports = {
     getOrder    : getOrder,
