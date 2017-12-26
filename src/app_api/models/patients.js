9f8d757f4791301a7d7ad9c6b86d8be9f9cebc64
MgenGlder <mgen.glder@gmail.com>
2017-12-26 18:34:00 -0500

    Changed type to String instead of Date because we don't need the time complexities that come with the Date type

--- src/app_api/models/patients.js
+++ src/app_api/models/patients.js
@@ -3,7 +3,7 @@ var mongoose = require("mongoose");
 var patientSchema = new mongoose.Schema({
     firstName: String,
     lastName: String,
-    dateOfBirth: Date
+    dateOfBirth: String
 })
 
 mongoose.model("Patient", patientSchema);
