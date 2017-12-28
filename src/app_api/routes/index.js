3581c72881a94ec3c3607df0b5d51bf21b7111e4
MgenGlder <mgen.glder@gmail.com>
2017-12-27 23:25:25 -0500

    Semantic updates to the routes of the api

--- src/app_api/routes/index.js
+++ src/app_api/routes/index.js
@@ -6,8 +6,13 @@ var ctrlOrders = require('../controllers/orders');
 
 router.get('/patient', ctrlPatients.getPatient);
 router.post('/patient', ctrlPatients.createPatient);
-router.get('/patients', ctrlPatients.getAllPatients);
+router.get('/patients/all', ctrlPatients.getAllPatients);
+router.patch('/patient', ctrlPatients.updatePatient);
+
 router.get('/order', ctrlOrders.getOrder);
 router.post('/order', ctrlOrders.createOrder);
-router.get('/orders', ctrlOrders.getAllOrders);
+router.post('/order/update', ctrlOrders.updateOrder);
+router.get('/orders/all', ctrlOrders.getAllOrders);
+router.get('/orders/patient/all', ctrlOrders.getAllOrdersSpecificPatient);
+
 module.exports = router;
