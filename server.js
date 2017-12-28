a0dcf3e11ed1e2ae1d19b2c4ba83b9cddc4c2132
MgenGlder <mgen.glder@gmail.com>
2017-12-27 23:23:25 -0500

    Added some commented out cors code. It didn't work but it normally would work had we had get methods inside of our main server file.

--- server.js
+++ server.js
@@ -2,17 +2,25 @@
 // require("dotenv").load();
 const express = require('express');
 const app = express();
+// app.use(function(res, req, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// })
 var path = require('path');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
 var fs = require('fs');
 require('./src/app_api/models/db');
 var routesApi = require('./src/app_api/routes/index');
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 
 app.use(express.static(__dirname + '/dist'));
 app.use("/api", routesApi);
