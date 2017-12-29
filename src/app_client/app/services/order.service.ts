4f325241d91a88e626e62bfa5ca01f9600308b02
MgenGlder <mgen.glder@gmail.com>
2017-12-28 22:25:50 -0500

    Adding functions to get orders from api to use on order view page

--- src/app_client/app/services/order.service.ts
+++ src/app_client/app/services/order.service.ts
@@ -1,29 +1,43 @@
 import { Injectable } from '@angular/core';
 import { Order } from '../models/pending-order.model'
-import {DBService } from './db.service';
+import { DBService } from './db.service';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class OrderService {
     public orders: Array<Order> = [];
     public visitingDoctor;
     public referrer;
-    constructor(private db: DBService) {}
+    public fetchedOrders: Observable<Response>;
+    constructor(private db: DBService, private http: Http) {
+        console.log("fetching all orders using the order service");
+        this.fetchedOrders = this.fetchAllOrders()
+    }
+    public getAllOrders(): Observable<Response> {
+        return this.fetchedOrders;
+    }
+
+    private fetchAllOrders() {
+        return this.http.get('http://localhost:8080/api/orders/all')
+    }
+
 
     addOrder(order: Order) {
         this.orders.push(order);
     }
 
-    removeOrder(toBeDeleted: Order){
+    removeOrder(toBeDeleted: Order) {
         //TODO: Might have to use string comparison
         console.log("removing order...");
         for (let i = 0; i < this.orders.length; i++) {
-            if(Object.is(this.orders[i], toBeDeleted)){
+            if (Object.is(this.orders[i], toBeDeleted)) {
                 this.orders.splice(i, 1);
                 console.log("order removed....");
             }
         }
     }
 
-    getOrders(){
+    getOrders() {
         return this.orders;
     }
     submitOrder(patientProfile) {
