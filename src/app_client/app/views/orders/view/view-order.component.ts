a1c10c1a3eb1e6d824d203e6175b566eb1d0b8e2
MgenGlder <mgen.glder@gmail.com>
2017-12-28 22:27:51 -0500

    Update to use observable atter to obtain patient and order information

--- src/app_client/app/views/orders/view/view-order.component.ts
+++ src/app_client/app/views/orders/view/view-order.component.ts
@@ -1,30 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Http, RequestOptions, RequestOptionsArgs, Response } from '@angular/http';
 import { PatientService } from "../../../services/patient.service";
+import { OrderService } from "../../../services/order.service";
 import { Observable } from "rxjs/Observable";
+import { Subscription } from "rxjs/Subscription";
 @Component({
   templateUrl: 'view-order.component.html'
 })
-export class ViewOrderComponent {
+export class ViewOrderComponent implements OnDestroy {
 
   public data;
   public filterQuery = '';
-  public patients: Observable<Response>;
+  public patientsSubscription: Subscription;
+  public patientsObservable: Observable<Response>;
   public patientData;
-  constructor(private http: Http, patientService: PatientService) {
+  public ordersObservable: Observable<Response>;
+  public ordersSubscription: Subscription;
+  public orderData;
+  constructor(private http: Http, patientService: PatientService, orderService: OrderService) {
     /*  this.http.get('./assets/patientData.json')
        .subscribe((data) => {
          this.data = data.json();
          console.log(this.data);
        }); */
-    this.patients = patientService.getAllPatients();
+    this.patientsObservable = patientService.getAllPatients();
 
-    this.patients
+    this.patientsSubscription = this.patientsObservable
       .map(data => data.json())
       .subscribe((patientData) => {
         this.patientData = patientData
         console.log(this.patientData);
       })
+
+    this.ordersObservable = orderService.getAllOrders();
+
+    this.ordersSubscription = this.ordersObservable
+      .map(data => data.json())
+      .subscribe((orderData) => {
+        this.orderData = orderData;
+        console.log(this.orderData);
+      })
+  }
+  
+  ngOnDestroy() {
+    this.patientsSubscription.unsubscribe();
+    this.ordersSubscription.unsubscribe();
+    console.log("destroyed in view order");
   }
 
   public toInt(num: string) {
