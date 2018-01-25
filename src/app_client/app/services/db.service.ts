fddce20e01e1960329db9d7e65880766e65b3f8f
MgenGlder <mgen.glder@gmail.com>
2018-01-25 01:31:29 -0500

    Updates to the api calls to make sure that different order types have different api calls taylored for the data they contain. Particularly, nurse and test.

--- src/app_client/app/services/db.service.ts
+++ src/app_client/app/services/db.service.ts
@@ -1,28 +1,89 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
+import { Order } from '../models/pending-order.model';
 @Injectable()
 export class DBService {
-    constructor(private http:Http) {
-      }
+    constructor(private http: Http) {
+    }
 
 
-    saveCompletePatientOrder(orders, patientProfile): Promise<any> {
+    saveCompletePatientOrder(orders: Array<any>, patientProfile): Promise<any> {
         console.log(patientProfile);
         console.log(orders);
+        let orderPromises = [];
         let order = orders[0];
+        for (order of orders) {
+            if (order.typeOfOrder == "nurse") {
+                this.http.post("http://localhost:8080/api/order", {
+                    patientFirstName: patientProfile.firstName,
+                    patientLastName: patientProfile.lastName,
+                    patientDateOfBirth: patientProfile.dateOfBirth,
+                    nursePurpose: order.nursePurpose,
+                    type: order.typeOfOrder,
+                    reason: order.reason,
+                    notes: order.notes,
+                    dateOfVisit: order.dateOfVisit,
+                    visitingDoctor: order.reporter,
+                    reporter: order.visitingDoctor,
+                    location: order.location
+                }).toPromise()
+            }
+            else if (order.typeOfOrder == "test") {
+                orderPromises.push(
+                    this.http.post("http://localhost:8080/api/order", {
+                        patientFirstName: patientProfile.firstName,
+                        patientLastName: patientProfile.lastName,
+                        patientDateOfBirth: patientProfile.dateOfBirth,
+                        type: order.type,
+                        testID: order.testID,
+                        reason: order.reason,
+                        notes: order.notes,
+                        dateOfVisit: order.dateOfVisit,
+                        visitingDoctor: order.visitingDoctor,
+                        reporter: order.reporter,
+                        location: order.location
+                    }).toPromise()
+                )
+            }
+            else if (order.typeOfOrder == "bloodwork") {
+                orderPromises.push(
+                    this.http.post("http://localhost:8080/api/order", {
+                        patientFirstName: patientProfile.firstName,
+                        patientLastName: patientProfile.lastName,
+                        patientDateOfBirth: patientProfile.dateOfBirth,
+                        type: order.typeOfOrder,
+                        testID: order.testID,
+                        reason: order.reason,
+                        notes: order.notes,
+                        dateOfVisit: order.dateOfVisit,
+                        visitingDoctor: order.visitingDoctor,
+                        report: order.reporter,
+                        location: order.location
+                    }).toPromise()
+                )
+            }
+            else {
+                orderPromises.push(
+                    this.http.post("http://localhost:8080/api/order", {
+                        patientFirstName: patientProfile.firstName,
+                        patientLastName: patientProfile.lastName,
+                        patientDateOfBirth: patientProfile.dateOfBirth,
+                        type: order.type,
+                        reason: order.reason,
+                        notes: order.notes,
+                        dateOfVisit: order.dateOfVisit,
+                        visitingDoctor: order.reporter,
+                        reporter: order.reporter,
+                        location: order.location
+                    }).toPromise()
+                )
+            }
+        }
         console.log("order saved");
-        return this.http.post("http://localhost:8080/api/order", {
-            patientFirstName: patientProfile.firstName,
-            patientLastName: patientProfile.lastName,
-            patientDateOfBirth: patientProfile.dateOfBirth,
-            type: order.type,
-            reason: order.reason,
-            notes: order.notes,
-            dateOfVisit: order.dateOfVisit,
-            visitingDoctor: order.reporter,
-            reporter: order.reporter,
-            location: order.location
-        }).toPromise();
+        return Promise.all(orderPromises);
+    }
+    saveCompletePatientTestOrder(orders, patientProfile) {
+        //TODO: Separate the different types of orders into their own functions.
     }
 
     getBloodworkOptions() {
@@ -66,17 +127,17 @@ export class DBService {
             { value: "physical-therapy", text: "Physical Therapy" }
         ]
     }
-   /*  getPatientList() {
-        return [
-            { firstName: "Joe", lastName: "Budden", dob: "09/11/1976" },
-            { firstName: "DJ", lastName: "Akademiks", dob: "03/12/1955" },
-            { firstName: "Nadeska", lastName: "Alexis", dob: "06/25/1963" },
-            { firstName: "Kunle", lastName: "Oshiyoye", dob: "07/30/1993" },
-            { firstName: "Justice", lastName: "Oshiyoye", dob: "01/27/1995" },
-            { firstName: "Brittany", lastName: "Oshiyoye", dob: "08/15/2002" },
-            { firstName: "Adekunle", lastName: "Oshiyoye", dob: "01/05/1951" },
-            { firstName: "Harold", lastName: "Pegues", dob: "06/20/1974" }
-        ]
-    } 
-    Depreciated, was used for testing purposes.*/
+    /*  getPatientList() {
+         return [
+             { firstName: "Joe", lastName: "Budden", dob: "09/11/1976" },
+             { firstName: "DJ", lastName: "Akademiks", dob: "03/12/1955" },
+             { firstName: "Nadeska", lastName: "Alexis", dob: "06/25/1963" },
+             { firstName: "Kunle", lastName: "Oshiyoye", dob: "07/30/1993" },
+             { firstName: "Justice", lastName: "Oshiyoye", dob: "01/27/1995" },
+             { firstName: "Brittany", lastName: "Oshiyoye", dob: "08/15/2002" },
+             { firstName: "Adekunle", lastName: "Oshiyoye", dob: "01/05/1951" },
+             { firstName: "Harold", lastName: "Pegues", dob: "06/20/1974" }
+         ]
+     } 
+     Depreciated, was used for testing purposes.*/
 }
