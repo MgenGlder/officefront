a52cb953b982716ed119b352e8b0bfd3e17795b1
Kunle Oshiyoye <mgenglder@Kunles-MacBook-Pro.local>
2018-06-10 19:08:18 -0400

    Start of creating tests

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+var expect = require('chai').expect;
+var request = require('supertest');
+var server = require('../server');
+
+function startServer() {
+    server();
+}
+
+describe('Our server', function() {
+    var app;
+    //TODO: Start building some tests for sanity checks.
+    // before(function(){
+    //     startServer();
+    // });
+    // it('should send back a json object after')
+})
