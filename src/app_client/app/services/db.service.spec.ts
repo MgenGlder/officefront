d34de0bdcb807069c79ff30fba78a2ac35328f58
Kunle Oshiyoye <mgen.glder@gmail.com>
2018-07-04 16:53:01 -0400

    Adding test for posting saved order object to the database.

--- src/app_client/app/services/db.service.spec.ts
+++ src/app_client/app/services/db.service.spec.ts
@@ -16,13 +16,13 @@ class SpyHttpAndClassToSpyOnClass {
     }
 }
 
-describe('DBService', () => {
+describe('DBService get request calls', () => {
     let service: DBService;
     let newHttp;
     beforeEach(() => {
         newHttp = new Http(new MockBackend(), new BaseRequestOptions());
     })
-    it('should make call to get bloodwork data from db', async() => {
+    it('should make call to get bloodwork data from db', async () => {
         SpyHttpAndClassToSpyOnClass.createHttpGetSpy(newHttp, [{ value: 'hgb-aic-level', text: 'Hgb. AIC Level' }])
         service = new DBService(newHttp);
         const result = service.getBloodworkOptions();
@@ -30,7 +30,7 @@ describe('DBService', () => {
             expect(responseObject.json()[0].value).toEqual('hgb-aic-level');
         }));
     })
-    it('should make call to get nursing data from db', async() => {
+    it('should make call to get nursing data from db', async () => {
         SpyHttpAndClassToSpyOnClass.createHttpGetSpy(newHttp, [{ value: 'rn-monitor-bp', text: 'Monitor BP' }])
         service = new DBService(newHttp);
         const result = service.getNurseOptions();
@@ -38,7 +38,7 @@ describe('DBService', () => {
             expect(responseObject.json()[0].value).toEqual('rn-monitor-bp');
         }))
     })
-    it('should make call to get testing data from db', async() => {
+    it('should make call to get testing data from db', async () => {
         SpyHttpAndClassToSpyOnClass.createHttpGetSpy(newHttp, [{ value: 'x-ray', text: 'X-Ray', location: true }])
         service = new DBService(newHttp);
         const result = service.getTestOptions();
@@ -46,12 +46,61 @@ describe('DBService', () => {
             expect(responseObject.json()[0].value).toEqual('x-ray');
         }))
     })
-    it('should make call to get specialist data from db', async() => {
+    it('should make call to get specialist data from db', async () => {
         SpyHttpAndClassToSpyOnClass.createHttpGetSpy(newHttp, [{ value: 'podiatrist', text: 'Podiatrist' }]);
         service = new DBService(newHttp);
         const result = service.getTestOptions();
         await result.subscribe(((responseObject: Response) => {
             expect(responseObject.json()[0].value).toEqual('podiatrist')
         }))
+    })
+})
+
+describe('DBService post calls', () => {
+    let newHttp;
+    let service: DBService;
+    beforeEach(() => {
+        newHttp = new Http(new MockBackend(), new BaseRequestOptions());
+    })
+    it('should make post calls to push new nursing order', () => {
+
+        const orders = [{
+            location: 'Right Arm',
+            visitingDoctor: 'Adler',
+            reporter: '',
+            dateOfVisit: '07/03/2018',
+            notes: 'Patient is very anxious',
+            reason: 'Sore throat',
+            typeOfOrder: 'nurse',
+            nursePurpose: 'rn-monitor-bp'
+        }]
+        const patientProfile = {
+            firstName: 'Kunle',
+            lastName: 'Oshiyoye',
+            dateOfBirth: '07/30/1993'
+        }
+        const postSpy = spyOn(newHttp, 'post');
+        postSpy.and.returnValue(
+            {
+                toPromise: () => Promise.resolve({
+                    message: 'Some successful response from post'
+                })
+            })
+        service = new DBService(newHttp);
+        const result = service.saveCompletePatientOrder(orders, patientProfile);
+        expect(postSpy.calls.count()).toEqual(1);
+        expect(postSpy.calls.argsFor(0)[1]).toEqual({
+            location: 'Right Arm',
+            visitingDoctor: 'Adler',
+            reporter: '',
+            dateOfVisit: '07/03/2018',
+            notes: 'Patient is very anxious',
+            reason: 'Sore throat',
+            typeOfOrder: 'nurse',
+            nursePurpose: 'rn-monitor-bp',
+            patientFirstName: 'Kunle',
+            patientLastName: 'Oshiyoye',
+            patientDateOfBirth: '07/30/1993'
+        })
     })
 })
--- src/app_client/app/services/db.service.ts
+++ src/app_client/app/services/db.service.ts
@@ -4,108 +4,113 @@ import { Order } from '../models/pending-order.model';
 import { Observable } from 'rxjs';
 import { request } from 'https';
 
+interface OrderRequestObject {
+    patientFirstName: String;
+    patientLastName: String;
+    patientDateOfBirth: String;
+    location: String;
+    visitingDoctor: String;
+    reporter: String;
+    dateOfVisit: String;
+    notes: String;
+    reason: String;
+    typeOfOrder: String;
+    nursePurpose?: String;
+    testID?: String;
+}
+
 @Injectable()
 export class DBService {
     constructor(private http: Http) {
     }
 
-    makePostCall(patientProfile, order) {
-        const requestObject: {
-            patientFirstName: String,
-            patientLastName: String,
-            patientDateOfBirth: String,
-            location: String,
-            visitingDoctor: String,
-            dateOfVisit: String,
-            notes: String,
-            reason: String,
-            type: String,
-            nursePurpose?: String,
-            testID?: String
-        } = {
-            patientFirstName: patientProfile.firstName,
-            patientLastName: patientProfile.lastName,
-            patientDateOfBirth: patientProfile.dateOfBirth,
-            location: order.location,
-            visitingDoctor: order.reporter,
-            dateOfVisit: order.dateOfVisit,
-            notes: order.notes,
-            reason: order.reason,
-            type: order.typeOfOrder,
-        }
-        switch (order.typeOfOrder) {
-            case 'nurse':
-                requestObject.nursePurpose = order.nursePurpose;
-                break;
-            case 'test':
-                requestObject.testID = order.testID;
-                break;
-            case 'bloodwork':
-                requestObject.testID = order.testID;
-                break;
-            case 'specialist':
-                break;
-            default:
-                break;
-        }
-        this.http.post('http://localhost:8080/api/order', requestObject).toPromise()
-    }
-    saveCompletePatientOrder(orders: Array<any>, patientProfile): Promise<any> {
-        const orderPromises = [];
-        let order = orders[0];
-        for (order of orders) {
-           this.makePostCall(patientProfile, order);
-        }
-        console.log('order saved');
-        return Promise.all(orderPromises);
-    }
 
-    getBloodworkOptions(): Observable<Response> {
-        // TODO: Set up to hit a real endpoint
-        return this.http.get('localhost:8080/some-endpoint-for-bloodwork');
-        // return [
-        //     { value: 'hgb-aic-level', text: 'Hgb. AIC Level' },
-        //     { value: 'bun-creat', text: 'BUN, CREAT' },
-        //     { value: 'cholesterol', text: 'Cholesterol/PSA' },
-        //     { value: 'lipid-profile', text: 'Lipid Profile' },
-        //     { value: 'cbc-with-diff', text: 'CBC With Diff' },
-        //     { value: 'comp-tsh-lft', text: 'COMP TSH LFT' },
-        //     { value: 'metabolic-panel', text: 'Metabolic Panel' }
-        // ];
+makePostCall(patientProfile, order) {
+    const requestObject: OrderRequestObject = {
+        patientFirstName: patientProfile.firstName,
+        patientLastName: patientProfile.lastName,
+        patientDateOfBirth: patientProfile.dateOfBirth,
+        location: order.location,
+        visitingDoctor: order.visitingDoctor,
+        dateOfVisit: order.dateOfVisit,
+        notes: order.notes,
+        reason: order.reason,
+        typeOfOrder: order.typeOfOrder,
+        reporter: order.reporter
     }
-    getNurseOptions(): Observable<Response> {
-        return this.http.get('localhost:8080/some-endpoint-for-nursing');
-        // return [
-        //     { value: 'rn-monitor-bp', text: 'Monitor BP' },
-        //     { value: 'rn-monitor-bs', text: 'Monitor BS' },
-        // ]
+    switch (order.typeOfOrder) {
+        case 'nurse':
+            requestObject.nursePurpose = order.nursePurpose;
+            break;
+        case 'test':
+            requestObject.testID = order.testID;
+            break;
+        case 'bloodwork':
+            requestObject.testID = order.testID;
+            break;
+        case 'specialist':
+            break;
+        default:
+            break;
     }
-    getTestOptions() {
-        return this.http.get('localhost:8080/some-endpoint-for-tests');
-        // return [
-        //     { value: 'x-ray', text: 'X-Ray', location: true },
-        //     { value: 'ekg', text: 'EKG', location: true },
-        //     { value: 'pft', text: 'PFT', location: false },
-        //     { value: 'eye-exam', text: 'Eye Exam', location: false },
-        //     { value: 'doppler', text: 'Doppler (Pedal Or Carotid)', location: true },
-        //     { value: 'ultrasound', text: 'Ultrasound', location: true },
-        //     { value: 'urology', text: 'Urology/Urinalysis', location: false }
-        // ];
+    return this.http.post('http://localhost:8080/api/order', requestObject).toPromise();
+}
+saveCompletePatientOrder(orders: Array < any > , patientProfile): Promise < any > {
+    const orderPromises = [];
+    let order = orders[0];
+    for (order of orders) {
+        orderPromises.push(this.makePostCall(patientProfile, order));
     }
-    getSpecialistOptions() {
-        return this.http.get('localhost:8080/some-endpoint-for-specialists')
-        // return [
-        //     { value: 'podiatrist', text: 'Podiatrist' },
-        //     { value: 'optometrist', text: 'Optometrist' },
-        //     { value: 'cardiologist', text: 'Cardiologist' },
-        //     { value: 'neurologist', text: 'Neurologist' },
-        //     { value: 'dermatologist', text: 'Dermatologist' },
-        //     { value: 'pain-doctor', text: 'Pain Doctor' },
-        //     { value: 'psychiatrist', text: 'Psychiatrist' },
-        //     { value: 'ent', text: 'ENT' },
-        //     { value: 'physical-therapy', text: 'Physical Therapy' }
-        // ]
-    }
+        console.log('order saved');
+    return Promise.all(orderPromises);
+}
+
+getBloodworkOptions(): Observable < Response > {
+    // TODO: Set up to hit a real endpoint
+    return this.http.get('localhost:8080/some-endpoint-for-bloodwork');
+    // return [
+    //     { value: 'hgb-aic-level', text: 'Hgb. AIC Level' },
+    //     { value: 'bun-creat', text: 'BUN, CREAT' },
+    //     { value: 'cholesterol', text: 'Cholesterol/PSA' },
+    //     { value: 'lipid-profile', text: 'Lipid Profile' },
+    //     { value: 'cbc-with-diff', text: 'CBC With Diff' },
+    //     { value: 'comp-tsh-lft', text: 'COMP TSH LFT' },
+    //     { value: 'metabolic-panel', text: 'Metabolic Panel' }
+    // ];
+}
+getNurseOptions(): Observable < Response > {
+    return this.http.get('localhost:8080/some-endpoint-for-nursing');
+    // return [
+    //     { value: 'rn-monitor-bp', text: 'Monitor BP' },
+    //     { value: 'rn-monitor-bs', text: 'Monitor BS' },
+    // ]
+}
+getTestOptions() {
+    return this.http.get('localhost:8080/some-endpoint-for-tests');
+    // return [
+    //     { value: 'x-ray', text: 'X-Ray', location: true },
+    //     { value: 'ekg', text: 'EKG', location: true },
+    //     { value: 'pft', text: 'PFT', location: false },
+    //     { value: 'eye-exam', text: 'Eye Exam', location: false },
+    //     { value: 'doppler', text: 'Doppler (Pedal Or Carotid)', location: true },
+    //     { value: 'ultrasound', text: 'Ultrasound', location: true },
+    //     { value: 'urology', text: 'Urology/Urinalysis', location: false }
+    // ];
+}
+getSpecialistOptions() {
+    return this.http.get('localhost:8080/some-endpoint-for-specialists')
+    // return [
+    //     { value: 'podiatrist', text: 'Podiatrist' },
+    //     { value: 'optometrist', text: 'Optometrist' },
+    //     { value: 'cardiologist', text: 'Cardiologist' },
+    //     { value: 'neurologist', text: 'Neurologist' },
+    //     { value: 'dermatologist', text: 'Dermatologist' },
+    //     { value: 'pain-doctor', text: 'Pain Doctor' },
+    //     { value: 'psychiatrist', text: 'Psychiatrist' },
+    //     { value: 'ent', text: 'ENT' },
+    //     { value: 'physical-therapy', text: 'Physical Therapy' }
+    // ]
+}
     /*  getPatientList() {
          return [
              { firstName: "Joe", lastName: "Budden", dob: "09/11/1976" },
