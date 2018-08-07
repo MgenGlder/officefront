0556143906943bb3fd6fc0935163cddd8fbbeb2d
Kunle Oshiyoye <mgen.glder@gmail.com>
2018-08-07 19:58:49 -0400

    Add a standin for the register endpoint

--- src/app_api/controllers/authentication.js
+++ src/app_api/controllers/authentication.js
@@ -17,7 +17,14 @@ function register(req, res) {
     // Use crypto for JWT, use bcrypt for password stuff. Bcrypt is more secure.
     let createdHash;
     let newUser;
+
+    if (req.body.username == undefined || req.body.email == undefined || req.body.password == undefined) {
+        sendJsonResponse(res, 404, {
+            response: "Need to send all parameters, username, email, and password"
+        })
+    }
     // TODO: Refactor this bad boy at some point
+<<<<<<< HEAD
     // User.findOne(req.body.username, (err, res) => {
         // Maybe we could use async await here??? Find out.
         // if(err) {
@@ -62,6 +69,44 @@ function register(req, res) {
         //     })
         //}
     // });
+=======
+    User.findOne({ username: req.body.username }, (err, data) => {
+        if (err) {
+            sendJsonResponse(res, 404, {
+                response: "There was an error creating the user"
+            });
+        } else if (!data) {
+            bcrypt.genSalt(11, function (err, salt) {
+                if (err) return;
+                bcrypt.hash(req.body.password, salt, function (err, hash) {
+                    if (err) return;
+                    createdHash = hash;
+                    newUser = new User({
+                        username: req.body.username,
+                        email: req.body.email,
+                        hash: createdHash,
+                    });
+                    newUser.save((err) => {
+                        if (err) {
+                            sendJsonResponse(res, 404, {
+                                response: "There was an error creating the user",
+                                err: err
+                            });
+                        } else {
+                            sendJsonResponse(res, 200, {
+                                response: "User created successfully"
+                            });
+                        }
+                    });
+                });
+            });
+        } else {
+            sendJsonResponse(res, 200, {
+                response: 'Username taken'
+            });
+        }
+    });
+>>>>>>> Add a standin for the register endpoint
 
 
 }
