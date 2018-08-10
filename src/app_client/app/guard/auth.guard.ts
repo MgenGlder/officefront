a3bc8875bedf9f97a8651d2e84832f4efe880c4c
Kunle Oshiyoye <mgen.glder@gmail.com>
2018-08-09 22:42:16 -0400

    Adding Auth0 package to help with JWT auth - Updating to use a real jwt token

--- src/app_client/app/guard/auth.guard.ts
+++ src/app_client/app/guard/auth.guard.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 @Injectable({ providedIn: 'root'}) // Service will be available to any module that is in the root
 // scope automagically. No need to import, be careful of this though.
 export class AuthGuard implements CanActivate {
-    constructor(private router: Router) { }
+    constructor(private router: Router) {
+     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        if (localStorage.getItem('currentUser')) {
+        const helper = new JwtHelperService();
+        if (localStorage.getItem('currentUser') || localStorage.getItem('currentUser') == 'null') {
+            const token = JSON.parse(localStorage.getItem('currentUser')).token;
+            const decodedToken = helper.decodeToken(token);
+            const expiredDate = helper.getTokenExpirationDate(token);
+            const isExpired = helper.isTokenExpired(token);
+            console.log(expiredDate);
+            console.log(isExpired);
+            console.log(decodedToken);
             return true;
         }
 
